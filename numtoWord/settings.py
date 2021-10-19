30b533b3ef22baa0a82c8ff7851aadaed1dadde0
abdulwasih <vasih95@gmail.com>
2021-10-19 21:00:40 +0530

    debug mode

--- numtoWord/settings.py
+++ numtoWord/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v1g^h06kpalg60%cp-&4u+r+z!)n+cnkwq^%-@g*)dri47uf62'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
